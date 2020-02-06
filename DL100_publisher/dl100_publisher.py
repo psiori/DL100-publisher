0727ea469724de145ae246d5480e8398fd8f261a
Axel Perschmann <ax.perschmann@gmail.com>
2020-02-06 12:11:36 +0100

    forward measurements directly via zmq as they are received from the scanner

--- DL100_publisher/dl100_publisher.py
+++ DL100_publisher/dl100_publisher.py
@@ -6,7 +6,7 @@ import sys
 import threading
 import time
 
-from typing import Dict, List, Tuple
+from typing import Dict, List, Optional, Tuple
 
 import numpy as np
 import zmq
@@ -16,15 +16,15 @@ from cpppo.server.enip import client, poll
 from cpppo.server.enip.get_attribute import attribute_operations, proxy_simple as device
 
 
-def values_to_bytes(values: Dict) -> bytes:
-    """
-    Extract relevant information and convert them to bytes.
-    """
-    ts = struct.pack(">d", values.get("ts"))
-    distance = struct.pack(">i", values.get("distance", np.nan))
-    velocity = struct.pack(">i", values.get("velocity", np.nan))
-
-    return ts + distance + velocity
+def str2bool(v: List[bool, str]):
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ("yes", "true", "t", "y", "1"):
+        return True
+    elif v.lower() in ("no", "false", "f", "n", "0"):
+        return False
+    else:
+        raise argparse.ArgumentTypeError("Boolean value expected.")
 
 
 class Publisher:
@@ -33,7 +33,11 @@ class Publisher:
     """
 
     def __init__(
-        self, host: str = "192.168.100.236", port: int = 44818, zmq_port: int = 5557
+        self,
+        host: str = "192.168.100.236",
+        port: int = 44818,
+        zmq_port: int = 5557,
+        verbose: bool = True,
     ):
         """Init method
 
@@ -41,48 +45,67 @@ class Publisher:
         host (str): The IP of the DL100 distance scanner
         port (int): The port used by the dl100 distance scanner
         zmq_port (int): The port used by zmq to publish values
+        verbose (bool): Activate verbose mode
         """
 
-        super().__init__()
-        self.stop = False
         self.host = host
-        self.port = port
+        self.port = port  # 44818: port for Ethernet IP
         self.zmq_port = zmq_port
+        self.verbose = verbose
 
-        self.poller = None
-        self.values = {}  # { <parameter>: (<timer>, <value>), ... }
+        self.zmq_active = False
+        self.pub_socket = None
 
-        self.keymap = {
-            ("@0x23/1/10", "DINT"): "distance",
-            ("@0x23/1/24", "DINT"): "velocity",
+        self.poller: Optional[threading.Thread] = None
+
+        self.setup_zmq()
+
+        self.keymap: Dict[Tuple[str, str], int] = {
+            ("@0x23/1/24", "DINT"): 2,  # velocity
+            ("@0x23/1/10", "DINT"): 1,  # distance
         }
 
-        self.start_data_polling(cycle=1 / 50)
+    def toggle_zmq_active(self):
+        if self.zmq_active:
+            self.zmq_active = False
+        else:
+            self.zmq_active = True
+        return self.zmq_active
 
-    def update_values(self, par: Tuple[str, str], val: List[float]):
-        self.values.update(
-            {
-                "ts": time.time(),  # use python ts, since we do not receive a ts from the dl100.
-                self.keymap[par]: val[0],
-            }
-        )
+    def callback_measurements(self, par: Tuple[str, str], val: List[float]):
+        ts = time.time()
+        val_type = self.keymap[par]
+
+        if self.zmq_active:
+            bytes = (
+                struct.pack(">d", ts)
+                + struct.pack(">i", val_type)
+                + struct.pack(">i", val[0])
+            )
+            self.pub_socket.send(bytes)
+
+            if self.verbose:
+                msg = f"Sending: {ts}, {val_type}, {val[0]}"
+                msg = msg + " " * (80 - len(msg))
+                sys.stdout.write("\r" + msg)
 
     def start_data_polling(self, cycle: float = 1 / 50):
         """
         Setup cpppo polling thread, reading the measurements from an Ethernet Connection to the Distance Scanner.
 
         Parameters:
-        cycle (float): The cycle length of polling measurments from the distance scanner
+        cycle (float): The cycle length for measurement polling
         """
-        self.values = {}
         self.poller = threading.Thread(
             target=poll.poll,
             kwargs={
                 "proxy_class": device,
-                "address": (self.host, self.port),  # 44818: port for Ethernet IP
+                "address": (self.host, self.port),
                 "cycle": cycle,
                 "timeout": 0.5,
-                "process": lambda par, val: self.update_values(par=par, val=val),
+                "process": lambda par, val: self.callback_measurements(
+                    par=par, val=val
+                ),
                 "params": list(self.keymap.keys()),
             },
         )
@@ -90,45 +113,20 @@ class Publisher:
         self.poller.start()
 
     def setup_zmq(self):
+        if self.pub_socket and not self.pub_socket.closed:
+            self.destroy_zmq()
+
         context = zmq.Context()
         self.pub_socket = context.socket(zmq.PUB)
         print("Publishing zmq msgs on port {port}".format(port=self.zmq_port))
         bind_addr = "tcp://*:{port}".format(port=self.zmq_port)
         self.pub_socket.bind(bind_addr)
+        self.zmq_active = True
 
     def destroy_zmq(self):
+        self.zmq_active = False
         self.pub_socket.close()
-        print(f"\nClosing port {self.zmq_port}")
-
-    def run(self, cycle: float = 1 / 30):
-        """
-        Opens a tcp socket and immediately starts sending measurement messages with specified cycletime.
-
-        Parameters:
-        cycle (float): The cycle length of measurements published via zmq
-        """
-        self.setup_zmq()
-        try:
-            a = 0
-            while True:
-                t0 = time.time()
-
-                if self.values:
-                    age = time.time() - self.values["ts"]
-
-                    if age > cycle:
-                        # only send recent values
-                        continue
-
-                    self.pub_socket.send(values_to_bytes(values=self.values.copy()))
-                    msg = f"Sending: {self.values}"
-                    msg = msg + " " * (80 - len(msg))
-                    sys.stdout.write("\r" + msg)
-
-                time.sleep(cycle - (time.time() - t0))
-        except KeyboardInterrupt:
-            self.destroy_zmq()
-            pass
+        print(f"Closing port {self.zmq_port}")
 
 
 def main():
@@ -157,18 +155,38 @@ def main():
     )
 
     parser.add_argument(
-        "--zmq_cycle",
+        "--cycle",
         type=float,
         required=False,
         default=1 / 30,
         help="The cycle length of measurements published via zmq",
     )
 
+    parser.add_argument(
+        "--verbose",
+        type=str2bool,
+        required=False,
+        default=True,
+        help="Activates verbose output",
+    )
+
     args = parser.parse_args()
     print(args)
 
-    pub = Publisher(host=args.dl100_ip, port=args.dl100_port, zmq_port=args.zmq_port)
-    pub.run(cycle=args.zmq_cycle)
+    pub = Publisher(
+        host=args.dl100_ip,
+        port=args.dl100_port,
+        zmq_port=args.zmq_port,
+        verbose=args.verbose,
+    )
+
+    pub.start_data_polling(cycle=args.cycle)
+
+    try:
+        while True:
+            time.sleep(5)
+    except KeyboardInterrupt:
+        pass
 
 
 if __name__ == "__main__":
